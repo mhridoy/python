4d1c8e3c0330780780d1dc5822c9705823487fb0
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2022-07-27 11:38:45 +0600

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+def hide():
+    
+
+
+    selected3 = option_menu(None, ["Home", "Targets",  "Scans", 'Settings','Login','Sign Up'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "rgb(255, 75, 75)"},
+        "icon": {"color": "white", "font-size": "25px"}, 
+        "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "rgb(255, 75, 75)"},
+    }
+)
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 3.52rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
